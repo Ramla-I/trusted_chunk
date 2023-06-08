427b94f8fe20f5e1db12beb79ce4baea35056e4c
ramla-i <ijazramla@gmail.com>
2023-06-08 14:35:41 -0400

    added INIT variable to prevent callback from being exposed more than once

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,14 @@
 #![feature(step_trait)]
 #![feature(rustc_private)]
 
-use trusted_chunk::TrustedChunk;
-
 #[macro_use]
 extern crate prusti_contracts;
 #[macro_use]
 extern crate cfg_if;
 extern crate core;
-// #[macro_use]
-// extern crate static_assertions;
-// mod memory_structs;
+
 pub(crate) mod spec;
+pub(crate) mod external_spec;
 pub mod linked_list;
 pub mod static_array;
 pub mod trusted_chunk;
@@ -22,23 +19,26 @@ pub mod trusted_chunk;
 
 cfg_if::cfg_if! {
 if #[cfg(prusti)] {
-    pub(crate) mod external_spec;
-    use crate::external_spec::{
-        trusted_range_inclusive::*,
-        trusted_option::*,
-        trusted_result::*,
-    };
+    use crate::external_spec::trusted_range_inclusive::*;
 } else {
     extern crate alloc;
     extern crate range_inclusive;
+    extern crate spin;
     use range_inclusive::*;
+
+    static INIT: spin::Once<bool> = spin::Once::new();
 }
 }
 
 
-#[cfg(not(prusti))] 
-pub fn init() -> fn(RangeInclusive<usize>) -> trusted_chunk::TrustedChunk {
-    create_from_unmapped
+#[cfg(not(prusti))] // prusti can't reason about fn pointers
+pub fn init() -> Result<fn(RangeInclusive<usize>) -> trusted_chunk::TrustedChunk, &'static str> {
+    if INIT.is_completed() {
+        Err("Trusted Chunk has already been initialized and callback has been returned")
+    } else {
+        INIT.call_once(|| true);
+        Ok(create_from_unmapped)
+    }
 }
 
 #[requires(*frames.start() <= *frames.end())]
