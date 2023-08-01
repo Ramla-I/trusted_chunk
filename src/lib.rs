3e14a2ee0ec646e6e938330cc97a5087c285d612
ramla-i <ijazramla@gmail.com>
2023-08-01 15:58:10 -0400

    Small changes, verifies

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ if #[cfg(prusti)] {
 
 
 #[cfg(not(prusti))] // prusti can't reason about fn pointers
-pub fn init() -> Result<fn(FrameRange) -> frame_chunk::FrameChunk, &'static str> {
+pub fn init_frame_chunk() -> Result<fn(FrameRange) -> frame_chunk::FrameChunk, &'static str> {
 
     if INIT.is_completed() {
         Err("Trusted Chunk has already been initialized and callback has been returned")
@@ -51,26 +51,4 @@ pub fn init() -> Result<fn(FrameRange) -> frame_chunk::FrameChunk, &'static str>
 fn create_from_unmapped(frames: FrameRange) -> frame_chunk::FrameChunk {
     frame_chunk::FrameChunk::trusted_new(frames)
 }
-
-// /// A macro for defining `PageRange` and `FrameRange` structs
-// /// and implementing their common traits, which are generally identical.
-// macro_rules! implement_page_frame_range {
-//     ($TypeName:ident, $desc:literal, $short:ident, $chunk:ident) => {
-//         paste! { // using the paste crate's macro for easy concatenation
-                        
-//             #[doc = "A range of [`" $chunk "`]s that are contiguous in " $desc " memory."]
-//             #[derive(Clone, PartialEq, Eq)]
-//             pub struct $TypeName(RangeInclusive<$chunk>);
-
-//             impl $TypeName {
-//                 #[doc = "Creates a new range of [`" $chunk "`]s that spans from `start` to `end`, both inclusive bounds."]
-//                 pub const fn new(start: $chunk, end: $chunk) -> $TypeName {
-//                     $TypeName(RangeInclusive::new(start, end))
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// implement_page_frame_range!(FrameRange, "physical", phys, Frame);
 
